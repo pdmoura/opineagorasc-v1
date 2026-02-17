1ffd055431f3607292a8b0ac3846c9bfcec512ed
pdmoura <sudwarriormoura@gmail.com>
2026-02-17 11:29:32 -0300

    fixed more bugs

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { createClient } from "@supabase/supabase-js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 const app = express();
 const port = process.env.PORT || 3000;
